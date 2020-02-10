7a54a70d51a2814d721ad0a71b3a0d41c7d91c4e
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-02-10 15:14:06 -0300

    AAA

--- src/App.js
+++ src/App.js
@@ -4,32 +4,50 @@ import "./App.css";
 function App() {
   return (
     <>
-
-    <div class="ui menu inverted fixed">
-      <h2 class="item ">Claudio Fundais</h2>
-      <button class="ui button primary">
-        criar projetos
-      </button>
-      <div class="ui focus input right menu massive">
-        <input type="text" placeholder="Procurar..." />
-      </div>
-    </div>
-
-    <div class="ui container three column grid">
-
-      <div class="column">
-        <div class="ui card">
-          <div class="image"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQe1uuFovZHMBlCjay9_KWo6DGvTiXlQ6UL-j3e-jWr6x27rRYy"/>  </div>
-          <div class="content">
-            <div class="header">Haru</div>
-            <div class="meta">Juntou-se em 2012</div>
-            <div class="description">Haru é uma youtuber que o bruno ama xDD.</div>
-          </div>
+      <div class="ui menu inverted fixed">
+        <h2 class="ui header item">ClaudioFundais</h2>
+        <span class="item">
+          <button class="ui animated blue basic inverted button ">
+            <span class="visible content">Criar Projeto</span>
+            <span class="hidden content">
+              <i aria-hidden="true" class="arrow right icon"></i>
+            </span>
+          </button>
+        </span>
+        <div class="ui inverted input item right">
+          <input type="text" placeholder="Procurar..." />
         </div>
       </div>
 
-    </div>
+      <div class="ui center aligned container grid ">
 
+        <div class="four wide colum">
+          
+          <div class="ui card">
+            <div class="image">
+              <img src="logo.svg"/>
+            </div>
+            <div class="content">
+              <div class="header">Elliot Baker</div>
+              <div class="meta">Friend</div>
+              <div class="description">
+                Elliot is a sound engineer living in Nashville who enjoys
+                playing guitar and hanging with his cat.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i aria-hidden="true" class="comment icon"></i>
+                15 Comentarios
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      
+        
+      </div>
     </>
   );
 }
